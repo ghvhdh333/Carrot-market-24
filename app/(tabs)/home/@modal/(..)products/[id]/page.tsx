b37cae1ae40bf4789b51af83a9126002060c64d2
김우주 <https://github.com/ghvhdh333>
2024-05-12 21:47:57 +0900

    feat/refreshBtn

--- app/(tabs)/home/@modal/(..)products/[id]/page.tsx
+++ app/(tabs)/home/@modal/(..)products/[id]/page.tsx
@@ -8,9 +8,15 @@
 // }
 
 import ModalCloseBtn from "@/components/buttons/modal-close-btn";
+import RefreshBtn from "@/components/buttons/refresh-btn";
 import db from "@/lib/db";
 import getSession from "@/lib/session/getSession";
 import { formatToWon } from "@/lib/utils";
+import {
+  ChatBubbleOvalLeftEllipsisIcon,
+  PencilSquareIcon,
+  TrashIcon,
+} from "@heroicons/react/24/outline";
 import { UserIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 import Link from "next/link";
@@ -60,6 +66,7 @@ export default async function Modal({ params }: { params: { id: string } }) {
   }
 
   const isOwner = await getIsOwner(product.userId);
+
   return (
     <div className="absolute w-full h-full z-50 flex flex-row items-center justify-center bg-opacity-60 left-0 top-0">
       <ModalCloseBtn />
@@ -75,23 +82,38 @@ export default async function Modal({ params }: { params: { id: string } }) {
                 alt={product.title}
               />
             </div>
-            <div className="p-5 flex items-center gap-3 border-b border-neutral-700">
-              <div className="size-10 rounded-full">
-                {product.user.avatar !== null ? (
-                  <Image
-                    src={product.user.avatar}
-                    width={40}
-                    height={40}
-                    alt={product.user.username}
-                    className="rounded-full"
-                  />
-                ) : (
-                  <UserIcon />
-                )}
+            <div className="p-5 flex flex-row justify-between border-b border-neutral-700">
+              <div className="flex flex-row items-center gap-3">
+                <div className="size-10 rounded-full">
+                  {product.user.avatar !== null ? (
+                    <Image
+                      src={product.user.avatar}
+                      width={40}
+                      height={40}
+                      alt={product.user.username}
+                      className="rounded-full"
+                    />
+                  ) : (
+                    <UserIcon />
+                  )}
+                </div>
+                <div>
+                  <h3>{product.user.username}</h3>
+                </div>
               </div>
-              <div>
-                <h3>{product.user.username}</h3>
-              </div>
+              {isOwner ? (
+                <div className="flex flex-row gap-4">
+                  <button className="bg-lime-600 px-5 py-2.5 rounded-md text-white font-semibold hover:bg-opacity-90">
+                    <PencilSquareIcon className="size-5" />
+                  </button>
+                  <button
+                    type="button"
+                    className="bg-red-500 px-5 py-2.5 rounded-md text-white font-semibold hover:bg-opacity-90"
+                  >
+                    <TrashIcon className="size-5" />
+                  </button>
+                </div>
+              ) : null}
             </div>
             <div className="p-5">
               <h1 className="text-2xl font-semibold">{product.title}</h1>
@@ -102,16 +124,12 @@ export default async function Modal({ params }: { params: { id: string } }) {
               <span className="font-semibold text-xl">
                 {formatToWon(product.price)} 원
               </span>
-              {isOwner ? (
-                <button className="bg-red-500 px-5 py-2.5 rounded-md text-white font-semibold">
-                  Delete product
-                </button>
-              ) : null}
+              <RefreshBtn />
               <Link
-                className="bg-orange-500 px-5 py-2.5 rounded-md text-white font-semibold"
+                className="bg-orange-500 px-10 py-2.5 rounded-md text-white font-semibold hover:bg-opacity-90"
                 href={``}
               >
-                Chatting
+                <ChatBubbleOvalLeftEllipsisIcon className="size-5" />
               </Link>
             </div>
           </div>
---
+++ components/buttons/refresh-btn.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { MagnifyingGlassPlusIcon } from "@heroicons/react/24/outline";
+
+const onClickRefreshButton = () => {
+  window.location.reload();
+};
+
+export default function RefreshBtn() {
+  return (
+    <button
+      type="button"
+      onClick={onClickRefreshButton}
+      className="bg-indigo-500 px-10 py-2.5 rounded-md text-white font-semibold hover:bg-opacity-90"
+    >
+      <MagnifyingGlassPlusIcon className="size-5" />
+    </button>
+  );
+}
