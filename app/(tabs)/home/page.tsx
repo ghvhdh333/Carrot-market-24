e008bbe105f737e5d447b18d0470e2e7f13bf718
김우주 <https://github.com/ghvhdh333>
2024-05-12 17:17:07 +0900

    feat/home-cache

--- app/(tabs)/home/page.tsx
+++ app/(tabs)/home/page.tsx
@@ -3,19 +3,25 @@ import db from "@/lib/db";
 import { PlusIcon } from "@heroicons/react/24/solid";
 import { Prisma } from "@prisma/client";
 import Link from "next/link";
-import { unstable_cache as nextCache, revalidatePath } from "next/cache";
+import { unstable_cache as nextCache } from "next/cache";
 
 export const metadata = {
   title: "home",
 };
 
 // 상품 리스트를 cache로 저장한다.
-// getInitialProducts 함수가 60초 이내에 재호출 시 cache에 저장된 기존 데이터를 보여주고,
-// getInitialProducts 함수가 60초 이후에 재호출 시 nextCache에 있는 함수(getInitialProducts)를 호출하여, 데이터를 갱신하고, 함수 호출 시 다시 revalidate 시간이 작동한다.
-const getCacheProducts = nextCache(getInitialProducts, ["home-products"]);
+// getInitialProducts 함수가 30초 이내에 재호출 시 cache에 저장된 기존 데이터를 보여주고,
+// getInitialProducts 함수가 30초 이후에 재호출 시 nextCache에 있는 함수(getInitialProducts)를 호출하여, 데이터를 갱신하고, 함수 호출 시 다시 revalidate 시간이 작동한다.
+const getCacheProducts = nextCache(getInitialProducts, ["home-products"], {
+  revalidate: 30,
+});
+
+// export const dynamic = 'force-dynamic';
+// export const revalidate = 60;
 
 async function getInitialProducts() {
   // take에 적힌 수 만큼 상품 리스트 가져옴
+  console.log("aaa");
   const products = await db.product.findMany({
     select: {
       title: true,
@@ -36,19 +42,11 @@ export type InitialProducts = Prisma.PromiseReturnType<
   typeof getInitialProducts
 >;
 
-const revalidate = async () => {
-  "use server";
-  revalidatePath("/home");
-};
-
 export default async function Products() {
   const initialProducts = await getCacheProducts();
   return (
     <div>
       <ProductList initialProducts={initialProducts} />
-      <form action={revalidate}>
-        <button>revalidate</button>
-      </form>
       <Link
         href="/add/products"
         className="bg-orange-500 flex items-center justify-center rounded-full size-16 fixed bottom-24 right-8 text-white transition-colors hover:bg-orange-400"
--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -2,16 +2,10 @@ import db from "@/lib/db";
 import getSession from "@/lib/session/getSession";
 import { formatToWon } from "@/lib/utils";
 import { UserIcon } from "@heroicons/react/24/solid";
+import { unstable_cache as nextCache, revalidateTag } from "next/cache";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
-
-export async function generateMetadata({ params }: { params: { id: string } }) {
-  const product = await getProduct(Number(params.id));
-  return {
-    title: product?.title,
-  };
-}
 
 // 쿠키에 있는 id가 제품을 업로드한 사용자의 id와 일치하는지 확인한다.
 async function getIsOwner(userId: number) {
@@ -24,6 +18,7 @@ async function getIsOwner(userId: number) {
 
 // 상품 정보와 등록한 유저의 정보를 가져온다.
 async function getProduct(id: number) {
+  console.log("product");
   const product = await db.product.findUnique({
     where: {
       id,
@@ -38,6 +33,32 @@ async function getProduct(id: number) {
     },
   });
   return product;
+}
+
+const getCachedProduct = nextCache(getProduct, ["product-detail"], {
+  tags: ["product-detail", "xxxx"],
+});
+
+async function getProductTitle(id: number) {
+  console.log("title");
+  const product = await db.product.findUnique({
+    where: { id },
+    select: {
+      title: true,
+    },
+  });
+  return product;
+}
+
+const getCachedProductTitle = nextCache(getProductTitle, ["product-title"], {
+  tags: ["product-title", "xxx"],
+});
+
+export async function generateMetadata({ params }: { params: { id: string } }) {
+  const product = await getCachedProductTitle(Number(params.id));
+  return {
+    title: product?.title,
+  };
 }
 
 export default async function ProductDetail({
@@ -55,12 +76,17 @@ export default async function ProductDetail({
 
   // DB에 해당 id값의 데이터가 있으면 가져오고,
   // 없으면 에러페이지로 이동한다.
-  const product = await getProduct(id);
+  const product = await getCachedProduct(id);
   if (!product) {
     return notFound();
   }
 
   const isOwner = await getIsOwner(product.userId);
+  const revalidate = async () => {
+    "use server";
+    revalidateTag("xxxx");
+    revalidateTag("product-title");
+  };
   return (
     <div className="pb-28">
       <div className="relative aspect-square">
@@ -93,6 +119,11 @@ export default async function ProductDetail({
         <h1 className="text-2xl font-semibold">{product.title}</h1>
         <p>{product.description}</p>
       </div>
+      <form action={revalidate}>
+        <button className="bg-red-500 px-5 py-2.5 rounded-md text-white font-semibold">
+          revalidate
+        </button>
+      </form>
       <div className="fixed w-full bottom-0 max-w-screen-sm p-5 pb-10 bg-neutral-800 flex justify-between items-center">
         <span className="font-semibold text-xl">
           {formatToWon(product.price)} 원
