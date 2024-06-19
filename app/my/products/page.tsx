a771725699afaf60bbb8942744f0bff71689a805
woojoo <ghvhdh333@gmail.com>
2024-06-20 00:35:02 +0900

    fix/button position

--- app/my/products/page.tsx
+++ app/my/products/page.tsx
@@ -57,7 +57,7 @@ export default async function MyProducts() {
           ))}
           <Link
             href={"/home"}
-            className="bg-orange-500 flex items-center justify-center rounded-full size-16 fixed bottom-24 right-8 text-white transition-colors hover:bg-orange-400"
+            className="bg-orange-500 flex items-center justify-center rounded-full size-16 fixed bottom-8 right-8 text-white transition-colors hover:bg-orange-400"
           >
             <HomeIcon className="size-10" />
           </Link>
